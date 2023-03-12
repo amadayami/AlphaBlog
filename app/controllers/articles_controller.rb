1a7ec92a1921390f5c3835e302d81e75f500c2e5
amadayami <amadayamichael@gmail.com>
2023-03-12 16:31:33 -0600

    added restrictions so user cannot edit/delete articles that do not belong to them.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     # byebug is used to stop execution and get a debugger console
@@ -47,6 +49,7 @@ class ArticlesController < ApplicationController
   #anything below private can only be seen by this file
   #it doesn't require an end
   private
+
   def set_article
     @article = Article.find(params[:id])
   end
@@ -54,4 +57,12 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article."
+      redirect_to @article
+    end
+  end
+
 end
